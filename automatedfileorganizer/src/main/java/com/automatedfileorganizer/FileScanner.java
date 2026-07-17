60def919e48a0ef1d139f5e2ecf03ee9e6c10455
Gamaliel <gamajverlp@gmail.com>
2026-07-17 11:11:48 -0600

    Remove scan method from FileScanner class to streamline code

--- automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileScanner.java
+++ automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileScanner.java
@@ -3,27 +3,4 @@ import java.io.File;
 
 public class FileScanner {
     
-    public void scan(String path){
-        File directory = new File(path);
-        
-        if(!directory.exists()){
-            System.out.println("Directory does not exist. / El directorio no existe.");
-        return;
-        }
-
-        File[] files = directory.listFiles();
-
-        if (files == null) {
-            System.out.println("Cannot read directory. / No se puede leer el directorio.");
-            return;
-        }
-
-        ExtensionMapper mapper = new ExtensionMapper();
-
-        for (File file : files) {
-            if (file.isFile()) {
-                System.out.println(file.getName() + " -> " + mapper.getCategory(file));
-            }
-        }
-    }
 }
