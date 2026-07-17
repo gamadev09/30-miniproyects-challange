a0b5f48204f33de7a6a372abbc2c2d21e62b19cd
Gamaliel <gamajverlp@gmail.com>
2026-07-16 20:10:07 -0600

    Fix class name from 'main' to 'Main' for proper Java naming conventions

--- automatedfileorganizer/src/main/java/com/automatedfileorganizer/main.java
+++ automatedfileorganizer/src/main/java/com/automatedfileorganizer/main.java
@@ -1,7 +1,7 @@
 package com.automatedfileorganizer;
 import java.util.Scanner;
 
-public class main{
+public class Main{
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
