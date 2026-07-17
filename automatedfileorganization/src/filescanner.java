a8ada27d7bf102f9417efed1b8e5bf12e5814afa
Gamaliel <gamajverlp@gmail.com>
2026-07-16 19:02:14 -0600

    Implement scan method in FileScanner class to check directory existence and list files

--- automatedfileorganization/src/filescanner.java
+++ automatedfileorganization/src/filescanner.java
@@ -1,4 +1,16 @@
 package automatedfileorganization.src;
+import java.io.File;
+
 public class FileScanner {
     
+    public void scan(String path){
+        File directory = new File(path);
+        
+        if(!directory.exists()){
+            System.out.println("Directory does not exist. / El directorio no existe.");
+        return;
+        }
+        File[] files = directory.listFiles();
+        
+    }
 }
