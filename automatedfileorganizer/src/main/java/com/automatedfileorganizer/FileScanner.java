a7bdae0784cc401df25a1c7b9bd088ed1664224e
Gamaliel <gamajverlp@gmail.com>
2026-07-17 11:21:30 -0600

    Enhance FileScanner to improve directory validation and error handling

--- automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileScanner.java
+++ automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileScanner.java
@@ -1,6 +1,34 @@
 package com.automatedfileorganizer;
 import java.io.File;
+import java.util.Objects;
 
 public class FileScanner {
-    
+
+    private final ExtensionMapper mapper;
+    public FileScanner(ExtensionMapper mapper){
+        this.mapper = Objects.requireNonNull(mapper);
+    }
+
+    public void scan(String path){
+        File directory = new File(path);
+        
+        if(!directory.exists()){
+            System.out.println("Directory does not exist. / El directorio no existe.");
+        return;
+        }
+
+        File[] files = directory.listFiles();
+
+        if (files == null) {
+            System.out.println("Cannot read directory. / No se puede leer el directorio.");
+            return;
+        }
+
+
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println(file.getName() + " -> " + mapper.getCategory(file));
+            }
+        }
+    }
 }
