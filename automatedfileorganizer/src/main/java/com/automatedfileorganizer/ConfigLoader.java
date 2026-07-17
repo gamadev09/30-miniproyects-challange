db48b30677416e7321268e046079d9528a1cd4fc
Gamaliel <gamajverlp@gmail.com>
2026-07-17 09:06:48 -0600

    Refactor ConfigLoader to improve category loading and enhance error handling

--- automatedfileorganizer/src/main/java/com/automatedfileorganizer/ConfigLoader.java
+++ automatedfileorganizer/src/main/java/com/automatedfileorganizer/ConfigLoader.java
@@ -1,26 +1,35 @@
 package com.automatedfileorganizer;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConfigLoader {
 
-    public Map<String, List<String>> loadCategories(){
+    public Map<String, List<String>> loadCategories() {
+
         ObjectMapper mapper = new ObjectMapper();
 
-    try {
-        return mapper.readValue(
-            new File("config/categories.json"),
-            new TypeReference<Map<String, List<String>>>() {}
-        );
-    }
-    catch(IOException e){
-        System.out.println("Cannot load categories file. / No se encuentra archivo de categorias");
-    }
-    return Map.of();
+        File file = new File("config/categories.json");
+
+        System.out.println("Path: " + file.getAbsolutePath());
+        System.out.println("Exists: " + file.exists());
+
+        try {
+            return mapper.readValue(
+                file,
+                new TypeReference<Map<String, List<String>>>() {}
+            );
+
+        } catch (IOException e) {
+            System.out.println("Cannot load categories file.");
+            e.printStackTrace(); // <-- Very useful while debugging
+        }
+
+        return Map.of();
     }
 }
