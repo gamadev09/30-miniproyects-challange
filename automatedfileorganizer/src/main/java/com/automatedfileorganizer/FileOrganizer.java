d4f24da49b478a7867483575c0d409d22e5f9f4d
Gamaliel <gamajverlp@gmail.com>
2026-07-17 11:58:45 -0600

    Refactor FileOrganizer to improve file organization logic and ensure category folder creation

--- automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileOrganizer.java
+++ automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileOrganizer.java
@@ -1,5 +1,27 @@
 package com.automatedfileorganizer;
+import java.io.File;
 
 public class FileOrganizer {
-    
+
+    public void organize(File file, String category){
+        File parentfolder = file.getParentFile();
+
+        File categoryfolder = new File(parentfolder, category);
+
+        if(!categoryfolder.exists()){
+            categoryfolder.mkdir();
+        }
+
+        File destination = new File(
+            categoryfolder,
+            file.getName()
+        );
+
+        if(file.renameTo(destination)){
+            System.out.println(
+                "Cannot move" + file.getName()
+            );
+        }
+    }
+
 }
