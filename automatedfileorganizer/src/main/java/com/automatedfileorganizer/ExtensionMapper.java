cc0fc24856943fb995fee70372c8a60aec71b1aa
Gamaliel <gamajverlp@gmail.com>
2026-07-17 10:54:22 -0600

    Implement category mapping in ExtensionMapper and update getCategory method

--- automatedfileorganizer/src/main/java/com/automatedfileorganizer/ExtensionMapper.java
+++ automatedfileorganizer/src/main/java/com/automatedfileorganizer/ExtensionMapper.java
@@ -1,8 +1,16 @@
 package com.automatedfileorganizer;
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 
 public class ExtensionMapper {
+    
+    private final Map<String, List<String>> categories;
+
+    public ExtensionMapper(Map<String, list<String>> categories){
+        this.categories = categories;
+    }
     public String getCategory(File file){
         
         String name = file.getName();
@@ -15,5 +23,16 @@ public class ExtensionMapper {
 
         String extension = name.substring(dotIndex).toLowerCase();
 
+        for(Map.Entry<String, List<String>> entry : categories.entrySet()){
+
+            String category = entry.getKey();
+            List<String> extensions = entry.getValue();
+            
+            if (extensions.contains(extension)) {
+                return category;
+            }
+        }
+        return "unkown";
+        }
     }
 }
