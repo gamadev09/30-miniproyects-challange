3bee2db2332cc67844f48788064449ad72516e86
Gamaliel <gamajverlp@gmail.com>
2026-07-16 18:06:04 -0600

    Refactor main method to handle user input for directory scanning and add ConfigLoader class

--- automatedfileorganization/src/ConfigLoader.java
+++ automatedfileorganization/src/ConfigLoader.java
@@ -1,5 +1,5 @@
 package automatedfileorganization.src;
 
-public class ConfiLoader {
+public class ConfigLoader {
     
 }
--- automatedfileorganization/src/main.java
+++ automatedfileorganization/src/main.java
@@ -6,19 +6,33 @@ public class main {
 
         Scanner input = new Scanner(System.in);
 
+        String directory;
+        FileScanner scanner = new FileScanner();
+        
         System.out.println("Automated File Organization");
         System.out.println("Please select:");
         System.out.println("1. English");
         System.out.println("2. Spanish");
         int languaje = input.nextInt();
+        input.nextLine();
         
         switch(languaje){
             case 1:
                 System.out.println("Please write your path to order");
-                String directory = input.nextLine();
-                FileScanner scanner = new FileScanner();
-        }
-
-
+                directory = input.nextLine();
+                scanner.scan(directory);
+            break;
+            case 2:
+                System.out.println("Porfavor ingresa el directorio a ordenar");
+                directory = input.nextLine();
+                scanner.scan(directory);
+            break;
+        default:
+            System.out.println("Invalid option/Opción Inválida");
+        break;
+            }
+            
+        input.close();
+        
     }
 }
