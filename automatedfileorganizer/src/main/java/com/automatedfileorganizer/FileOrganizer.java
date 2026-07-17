b86c6849651eac81fb194c7be339ab29d53f9361
Gamaliel <gamajverlp@gmail.com>
2026-07-17 12:31:22 -0600

    Refactor FileOrganizer to improve file handling and add error handling for existing files

--- automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileOrganizer.java
+++ automatedfileorganizer/src/main/java/com/automatedfileorganizer/FileOrganizer.java
@@ -1,12 +1,28 @@
 package com.automatedfileorganizer;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class FileOrganizer {
 
     public void organize(File file, String category){
+
+        if(category.equals("unknown")){
+            System.out.println(
+                "Skipped unknown file. / Archivo desconocido omitido: "
+                + file.getName()
+            );
+            return;
+        }
+
         File parentFolder = file.getParentFile();
 
-        File categoryFolder = new File(parentFolder, category);
+        File categoryFolder = new File(
+            parentFolder,
+            category
+        );
 
         if(!categoryFolder.exists()){
             categoryFolder.mkdirs();
@@ -17,16 +33,36 @@ public class FileOrganizer {
             file.getName()
         );
 
-        if(file.renameTo(destination)){
+        if(destination.exists()){
             System.out.println(
-                file.getName() + " moved to. / se movio a. " + category
+                "File already exists. / El archivo ya existe: "
+                + file.getName()
             );
+            return;
         }
-        else{
-            System.out.println(
-                "Cannot move. / No se puede mover. " + file.getName()
+
+        try {
+
+            Files.move(
+                file.toPath(),
+                destination.toPath(),
+                StandardCopyOption.REPLACE_EXISTING
             );
+
+            System.out.println(
+                file.getName()
+                + " moved to / se movio a "
+                + category
+            );
+
+        } catch (IOException e){
+
+            System.out.println(
+                "Cannot move / No se puede mover: "
+                + file.getName()
+            );
+
+            e.printStackTrace();
         }
     }
-
 }
